aef19b847d99e555d5981fbe6887e3c1375d7d57
Georg Sawtschuk <georg.the.moon@googlemail.com>
2017-05-04 09:09:09 +0200

    добавил параметр для просмотрщика

--- src/browser.js
+++ src/browser.js
@@ -1,7 +1,26 @@
 
 function recoverData() {
-    let dobrobot = document.getElementById("dobrobot");
-    dobrobot.value = localStorage.getItem("dobrobot");
+    
+    let recover = true;
+	//console.log("updateValues = " + JSON.stringify(site));     
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    
+    if(vars["dobrobot"]) {
+        recover = false;
+        document.getElementById("dobrobot").value = vars["dobrobot"];
+    }    
+    
+    if(recover) {
+        let dobrobot = document.getElementById("dobrobot");
+        dobrobot.value = localStorage.getItem("dobrobot");
+    }
 }
 
 
